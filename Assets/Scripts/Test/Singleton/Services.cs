5419689776bac573120373e7bcb73578e7e7283a
NEDNAR <drakozavrov20@gmail.com>
2020-12-20 19:25:54 +0300

    Singleton & Service locator training (temporal)

---
+++ Assets/Scripts/Test/Singleton/Services.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public sealed class Services
+    {
+        private static readonly Lazy<Services> _instance =
+            new Lazy<Services>(() => new Services(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        public static Services Instance => _instance.Value;
+
+        public void Test()
+        {
+            Debug.Log(nameof(Services));
+        }
+    }
+}
